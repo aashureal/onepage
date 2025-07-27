724b144663fe724352072c2ee972868d6e88fdda
Aashutosh Uikey <aashureal01@gmail.com>
2025-07-27 20:36:51 +0530

    Granger - Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { Helmet } from "react-helmet";
 
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Home / Onepage</title>
+      </Helmet>
+    </>
+  );
+};
 export default Home;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Test from "../pages/Test/Test";
+import Granger from "../pages/Granger/Granger";
 
 const AppRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
+        <Route path="/Granger" element={<Granger />} />
       </Routes>
     </>
   );
